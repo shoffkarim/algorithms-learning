4deaf5fc8b55cb0938bb005e603a61bec78654f7
Karim Sharafutdinov <karimshoff@gmail.com>
2024-06-24 09:51:41 +0500

    determine if two strings are close

---
+++ tasks/determine-if-two-strings-are-close/index.js
@@ -0,0 +1,41 @@
+const word1 = 'cabbba'
+const word2 = 'abbccc'
+
+const closeStrings = (word1, word2) => {
+  if(word1.length !== word2.length) {
+    return false
+  }
+
+  const freq1 = new Array(26).fill(0)
+  const freq2 = new Array(26).fill(0)
+
+  for(let i = 0; i < word1.length; i++) {
+    freq1[word1.charCodeAt(i) - 'a'.charCodeAt(0)]++
+  }
+
+  for(let i = 0; i < word2.length; i++) {
+    freq2[word2.charCodeAt(i) - 'a'.charCodeAt(0)]++
+  }
+
+
+  for(let i = 0; i < 26; i++) {
+    if((freq1[i] > 0 && freq2[i] === 0) || (freq2[i] > 0 && freq1[i] === 0)) {
+      return false
+    }
+  }
+
+
+  freq1.sort((a, b) => a - b)
+
+  freq2.sort((a, b) => a - b)
+
+  for(let i = 0; i < 26; i++) {
+    if(freq1[i] !== freq2[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(closeStrings(word1, word2))
